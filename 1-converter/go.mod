53d734095d3834caa8ad66cbe5de5f60578b0cf8
Abdurahman Abdurahman <unilife.ch@gmail.com>
2024-11-25 21:56:23 +0500

    1-start

---
+++ 1-converter/go.mod
@@ -0,0 +1,3 @@
+module coverter/app
+
+go 1.23.3
--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,1 +1,11 @@
-package __converter
+package main
+
+import "fmt"
+
+func main() {
+	const usdEur float64 = 0.95
+	const usdRub float64 = 103
+	const eurRub float64 = usdRub / usdEur
+	fmt.Println(eurRub)
+
+}
